94a439caa688590e530c47a90fcbb78cb0c6ab45
Bảo Ngọc <ngoc.olivia2212257@gmail.com>
2025-10-23 21:46:20 +0700

    swagger docs API

--- Backend/src/main/java/com/travollo/Travel/TravelApplication.java
+++ Backend/src/main/java/com/travollo/Travel/TravelApplication.java
@@ -1,17 +1,9 @@
 package com.travollo.Travel;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Travello API",
-                version = "1.0",
-                description = "API documentation for platform Travello"
-        )
-)
 @SpringBootApplication
 public class TravelApplication {
 
