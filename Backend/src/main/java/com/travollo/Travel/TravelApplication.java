ba066e9ba9c0116f6416d1205bfea8578bf3cd33
Bảo Ngọc <ngoc.olivia2212257@gmail.com>
2025-12-27 02:59:11 +0700

    cors

--- Backend/src/main/java/com/travollo/Travel/TravelApplication.java
+++ Backend/src/main/java/com/travollo/Travel/TravelApplication.java
@@ -3,8 +3,9 @@ package com.travollo.Travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientWebSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {OAuth2ClientWebSecurityAutoConfiguration.class})
 public class TravelApplication {
 
 	public static void main(String[] args) {
