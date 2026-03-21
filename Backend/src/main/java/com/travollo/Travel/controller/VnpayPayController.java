e87df8d76b310b8a1920a732cb36573ad61e8cf4
Bảo Ngọc <ngoc.olivia2212257@gmail.com>
2026-03-21 22:42:43 +0700

    refactor: update

--- Backend/src/main/java/com/travollo/Travel/controller/VnpayPayController.java
+++ Backend/src/main/java/com/travollo/Travel/controller/VnpayPayController.java
@@ -59,39 +59,5 @@ public class VnpayPayController {
         }
     }
 
-    @GetMapping(value = "/result")
-    public Map<String, String> completePayment(HttpServletRequest request,
-                                               @RequestParam(name = "vnp_OrderInfo") String vnp_OrderInfo,
-                                               @RequestParam(name = "vnp_Amount") Integer vnp_Amount,
-                                               @RequestParam(name = "vnp_BankCode", defaultValue = "") String vnp_BankCode,
-                                               @RequestParam(name = "vnp_BankTranNo") String vnp_BankTranNo,
-                                               @RequestParam(name = "vnp_CardType") String vnp_CardType,
-                                               @RequestParam(name = "vnp_PayDate") String vnp_PayDate,
-                                               @RequestParam(name = "vnp_ResponseCode") String vnp_ResponseCode,
-                                               @RequestParam(name = "vnp_TransactionNo") String vnp_TransactionNo,
-                                               @RequestParam(name = "vnp_TxnRef") String vnp_TxnRef
-    ) {
-        Map<String, String> response = new HashMap<>();
 
-        String year = vnp_PayDate.substring(0, 4);
-        String month = vnp_PayDate.substring(4, 6);
-        String date = vnp_PayDate.substring(6, 8);
-        String hour = vnp_PayDate.substring(8, 10);
-        String minutes = vnp_PayDate.substring(10, 12);
-        String second = vnp_PayDate.substring(12, 14);
-
-        String timePay = date + "-" + month + "-" + year + " " + hour + ":" + minutes + ":" + second;
-
-        response.put("vnp_OrderInfo", vnp_OrderInfo);
-        response.put("vnp_Amount", vnp_Amount.toString());
-        response.put("vnp_BankCode", vnp_BankCode);
-        response.put("vnp_BankTranNo", vnp_BankTranNo);
-        response.put("vnp_CardType", vnp_CardType);
-        response.put("vnp_PayDate", timePay);
-        response.put("vnp_ResponseCode", vnp_ResponseCode);
-        response.put("vnp_TransactionNo", vnp_TransactionNo);
-        response.put("vnp_TxnRef", vnp_TxnRef);
-
-        return response;
-    }
 }
