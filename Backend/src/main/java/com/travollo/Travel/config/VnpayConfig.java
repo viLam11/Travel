1a1af2b2bfbbe1d083887b9c60320abc8338ed00
Bảo Ngọc <ngoc.olivia2212257@gmail.com>
2026-03-21 22:42:17 +0700

    refactor: remove redudant codes

--- Backend/src/main/java/com/travollo/Travel/config/VnpayConfig.java
+++ Backend/src/main/java/com/travollo/Travel/config/VnpayConfig.java
@@ -24,50 +24,11 @@ public class VnpayConfig {
     @Value("${vnp.api_url}")
     public String vnp_apiUrl;
 
-//    public static String md5(String message) {
-//        String digest = null;
-//        try {
-//            MessageDigest md = MessageDigest.getInstance("MD5");
-//            byte[] hash = md.digest(message.getBytes("UTF-8"));
-//            StringBuilder sb = new StringBuilder(2 * hash.length);
-//            for (byte b : hash) {
-//                sb.append(String.format("%02x", b & 0xff));
-//            }
-//            digest = sb.toString();
-//        } catch (UnsupportedEncodingException ex) {
-//            digest = "";
-//        } catch (NoSuchAlgorithmException ex) {
-//            digest = "";
-//        }
-//        return digest;
-//    }
-
     public static String Sha256(String message) {
         String digest = null;
 
         return digest;
     }
-
-//    public static String hashAllFields(Map<String, String> fields) throws UnsupportedEncodingException {
-//        List<String> fieldNames = new ArrayList<>(fields.keySet());
-//        Collections.sort(fieldNames);
-//        StringBuilder sb = new StringBuilder();
-//        sb.append(VnpayConfig.vnp_HashSecret);
-//        Iterator<String> itr = fieldNames.iterator();
-//        while (itr.hasNext()) {
-//            String fieldName = (String) itr.next();
-//            String fieldValue = (String) fields.get(fieldName);
-//            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-//                sb.append(fieldName);
-//                sb.append("=");
-//                sb.append(URLDecoder.decode(fieldValue,"UTF-8"));
-//            }
-//            if (itr.hasNext()) {
-//                sb.append("&");
-//            }
-//        }
-//        return Sha256(sb.toString());
-//    }
 
     public static String getIpAddress(HttpServletRequest request) {
         String ipAdress;
