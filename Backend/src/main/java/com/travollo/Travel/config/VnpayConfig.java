f401b06ccab01f494731c4f710ab06659ed72913
Bảo Ngọc <ngoc.olivia2212257@gmail.com>
2026-03-21 22:40:53 +0700

    feat: update features

--- Backend/src/main/java/com/travollo/Travel/config/VnpayConfig.java
+++ Backend/src/main/java/com/travollo/Travel/config/VnpayConfig.java
@@ -44,21 +44,7 @@ public class VnpayConfig {
 
     public static String Sha256(String message) {
         String digest = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(message.getBytes("UTF-8"));
-            StringBuilder sb = new StringBuilder(2 * hash.length);
-            for (byte b : hash) {
-                sb.append(String.format("%02x", b & 0xff));
-            }
 
-            digest = sb.toString();
-
-        } catch (UnsupportedEncodingException ex) {
-            digest = "";
-        } catch (NoSuchAlgorithmException ex) {
-            digest = "";
-        }
         return digest;
     }
 
